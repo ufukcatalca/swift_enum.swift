21b1acd917dbc478159b6051c56ae643a746f183
Ufuk Çatalca <ucatalca@thy.com>
2018-11-05 08:10:18 +0300

    enum files added

---
+++ swift_enum.swift
@@ -0,0 +1,14 @@
+public enum Months{
+    case January
+    case February
+    case March
+    case April 
+    case May
+    case June
+    case July
+    case August
+    case September
+    case October
+    case November
+    case December
+}
